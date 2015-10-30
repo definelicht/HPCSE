4bd905bf15e22cd113350c3cbeea82351a1b233a
Johannes de Fine Licht <johannes.definelicht@cern.ch>
2015-10-30 19:02:36 +0100

    Sweep script for exercise 6.

---
+++ exercise6/sweep.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+import os, sys
+
+grids = [(128, 1e-5, 10), (256, 1e-6, 0.5), (1024, 1e-8, 0.0002)]
+cores = [1, 6, 12, 18, 24, 30, 36, 42, 48]
+
+for (dim, dt, end) in grids:
+  for c in cores:
+    outputStr = str(dim) + "_" + str(c) + ".txt";
+    command = "mpirun -n {} exercise6/RunDiffusionMPI 1 {} {} {} {}".format(
+        c, dim, dt, outputStr, end)
+    # Warmup round
+    os.system(command + " >> /dev/null")
+    for i in range(3):
+      os.system(command)
