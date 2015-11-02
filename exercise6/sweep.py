81bf105c37d9b90eca25469231ab6a98ee022197
Johannes de Fine Licht <johannes.definelicht@cern.ch>
2015-11-02 10:41:44 +0100

    Updated sweep script for Euler.

--- exercise6/sweep.py
+++ exercise6/sweep.py
@@ -1,8 +1,11 @@
 #!/usr/bin/env python3
 import os, sys
 
-grids = [(128, 1e-5, 10), (256, 1e-6, 0.5), (1024, 1e-8, 0.0002)]
-cores = [1, 6, 12, 18, 24, 30, 36, 42, 48]
+grids = [(128, 1e-5, 250), (256, 1e-6, 6.25), (1024, 1e-8, 0.002)]
+if len(sys.argv) < 2:
+  cores = [1, 6, 12, 18, 24, 30, 36, 42, 48]
+else:
+  cores = [int(x) for x in sys.argv[1:]]
 
 for (dim, dt, end) in grids:
   for c in cores:
